a1879820de3bdf2fa2bec584901f0ad62fddde67
Akinade Toheeb Adebusoye <adebusoyeteeman@gmail.com>
2023-11-10 04:21:54 +0100

    t

--- src/main/java/net/akinadetoheeb/concurrent_pancakes/ConcurrentPancake.java
+++ src/main/java/net/akinadetoheeb/concurrent_pancakes/ConcurrentPancake.java
@@ -1,55 +1,112 @@
 package net.akinadetoheeb.concurrent_pancakes;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 // Concurrent Java Program that simulates the shopkeeper scenario
 public class ConcurrentPancake {
-        public static void main(String[] args) {
-            final int maxPancakesPerUser = 5;
-            final int maxPancakesByShopkeeper = 12;
-            final int numUsers = 3;
-            int totalPancakesMade = 0;
-            int totalPancakesEaten = 0;
-            int totalUnmetOrders = 0;
-            int totalWastedPancakes = 0;
 
-            // System will decide At Most 12 pancakes made by Shopkeeper in 30s
-            Random randomPancakes = new Random();
+    public static void main(String[] args) {
+        final int maxPancakesPerUser = 5;
+        final int maxPancakesByShopkeeper = 12;
+        final int numUsers = 3;
 
-            for (int time = 1; time <= 5; time++)
+        int totalPancakesMade = 0;
+        int totalPancakesEaten = 0;
+
+
+        // Creates an array of integers with a single element, initialized to 0
+        final int[] totalMetPancakes ={0};
+        final int[] totalUnmetPancakes = {0};
+
+        // System will decide At Most 12 pancakes made by Shopkeeper in 30s
+        Random randomPancakes = new Random();
+
+        for (int time = 1; time <= 5; time++) {
+
+            // Generates random integers of pancakes between 1 and 12, and the range includes both 1 and 12.
+            int pancakesMadeByShopkeeper = randomPancakes.nextInt(maxPancakesByShopkeeper) + 1;
+
+
+            // Using CompletableFuture to represent the asynchronous computation for each user
+            CompletableFuture<Integer>[] users = new CompletableFuture[numUsers];
+            for (int user = 0; user < numUsers; user++)
             {
-                // Generates random integers between 1 and 12, and the range includes both 1 and 12.
-                int pancakesMade = randomPancakes.nextInt(maxPancakesByShopkeeper) + 1;
 
-                int pancakesEaten = 0;
-                int unmetOrders = 0;
+                users[user] = CompletableFuture.supplyAsync(() ->
+                {
+                    // Generates random integers of pancakes eaten by User between 1 and 5
+                    int pancakesOrderPerUser = randomPancakes.nextInt(maxPancakesPerUser) + 1;
 
-                for (int user = 0; user < numUsers; user++) {
-                    pancakesEaten = new Random().nextInt(1,maxPancakesPerUser + 1);
-                    if (pancakesEaten > pancakesMade) {
-                        unmetOrders++;
-                        totalWastedPancakes += pancakesEaten - pancakesMade;
+                    // User Request, Unmet Order by Shopkeeper
+                    if (pancakesOrderPerUser > pancakesMadeByShopkeeper)
+                    {
+                        // Unmet Pancakes by the Shopkeeper
+                        totalUnmetPancakes[0] +=  pancakesOrderPerUser - pancakesMadeByShopkeeper;
+
+                        // User couldn't eat any pancakes due to unmet order
+                        return 0;
+
                     }
-                    pancakesMade -= pancakesEaten;
-                    totalPancakesEaten += pancakesEaten;
-                }
 
-                totalPancakesMade += pancakesMade;
-                totalUnmetOrders += unmetOrders;
+                       // User Request, Order Met by Shopkeeper
+                    else
+                    {
+                        // Pancakes Met by the Shopkeeper
+                        totalMetPancakes[0] += pancakesOrderPerUser;
 
-                System.out.println("30-Second time Slot #" + time);
-                System.out.println("Starting Time of making the pancakes: " + ((time - 1) * 30) + " seconds");
-                System.out.println("Ending Time of making the pancakes: " + (time * 30) + " seconds");
-                System.out.println("Pancakes Made by Shopkeeper: " + pancakesMade);
-                System.out.println("Pancakes Eaten by Users: " + totalPancakesEaten);
-                System.out.println("Unmet Orders: " + unmetOrders);
-                System.out.println("Wasted Pancakes: " + (pancakesEaten - pancakesMade));
-                System.out.println();
+                        // User eats Pancakes that was Met by Shopkeeper
+                        return pancakesOrderPerUser;
+                    }
+                });
             }
 
-            System.out.println("Total Pancakes Made: " + totalPancakesMade);
-            System.out.println("Total Pancakes Eaten: " + totalPancakesEaten);
-            System.out.println("Total Unmet Orders: " + totalUnmetOrders);
-            System.out.println("Total Wasted Pancakes: " + totalWastedPancakes);
+            // Combine all user and get the sum of pancakes eaten
+            CompletableFuture<Void> allOf = CompletableFuture.allOf(users);
+            try {
+                allOf.get(); // Wait for all user computations to complete
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+
+            // A Total Output of Five Different Time Slot Interval
+            int pancakesEatenByUsers = 0;
+            int unmetOrders = 0;
+
+            for (CompletableFuture<Integer> user : users) {
+                try {
+                    pancakesEatenByUsers += user.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Total Outputs of Five Different Time Slot Interval
+            totalPancakesMade += pancakesMadeByShopkeeper;
+            totalPancakesEaten += pancakesEatenByUsers;
+
+
+            // Five Different Time Slot Interval
+            System.out.println("30-Second time Slot #" + time);
+            System.out.println("Starting Time of making the pancakes: " + ((time - 1) * 30) + " seconds");
+            System.out.println("Ending Time of making the pancakes: " + (time * 30) + " seconds");
+            System.out.println("Pancakes Made by Shopkeeper: " + pancakesMadeByShopkeeper);
+            System.out.println("Pancakes Eaten by a Single User: " + pancakesEatenByUsers);
+
+            // Math.max function to ensure that the result is non-negative
+            System.out.println("Wasted Pancakes: " + (Math.max(0, pancakesMadeByShopkeeper - pancakesEatenByUsers)));
+            System.out.println();
+
+
         }
+
+        // (Total) Output of Five Different Time Slot
+        System.out.println("Total Pancakes Made by Shopkeeper: " + totalPancakesMade);
+        System.out.println("Total Pancakes Eaten by Users: " + totalPancakesEaten);
+        System.out.println("Total Met Orders: " + totalMetPancakes[0]);
+        System.out.println("Total Unmet Pancakes by Shopkeeper: " + totalUnmetPancakes[0]);
+
+    }
 }
--- src/main/java/net/akinadetoheeb/non_concurrent_pancakes/NonConcurrentPancake.java
+++ src/main/java/net/akinadetoheeb/non_concurrent_pancakes/NonConcurrentPancake.java
@@ -1,6 +1,8 @@
 package net.akinadetoheeb.non_concurrent_pancakes;
 
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 // Non-concurrent Java Program that simulates the shopkeeper scenario
 public class NonConcurrentPancake {
@@ -11,17 +13,14 @@ public class NonConcurrentPancake {
         int totalPancakesMade = 0;
         int totalPancakesEaten = 0;
         int totalUnmetOrders = 0;
-        int totalWastedPancakes = 0;
-
-        // System will decide At Most 12 pancakes made by Shopkeeper in 30s
-        Random randomPancakes = new Random();
+        final int[] totalWastedPancakes = {0};
 
 
         // Shopkeeper Five Different Time Slot Interval for making the pancakes
         for (int time = 1; time <= 5; time++)
         {
             // Generates random integers between 1 and 12, and the range includes both 1 and 12.
-            int numbOfPancakes = randomPancakes.nextInt (maxPancakesByShopkeeper) + 1;
+            int numbOfPancakes = new Random().nextInt(maxPancakesByShopkeeper) + 1;
 
 
             int[] pancakesEaten = new int[numOfUsers];
@@ -29,40 +28,48 @@ public class NonConcurrentPancake {
 
             for (int user = 0; user < numOfUsers; user++) // Indicates presence of three users(0,1,2)
             {
-                //
-                //PancakesEaten by a Single User : 2
-                pancakesEaten[user] = randomPancakes.nextInt(1,maxPancakesPerUser + 1); // 2
 
-                if (pancakesEaten[user] > numbOfPancakes)  // 2>1
+                // Generates random integers of pancakes eaten by User between 1 and 5
+                pancakesEaten[user] = new Random().nextInt(maxPancakesPerUser) + 1; // 2
+
+                if (pancakesEaten[user] > numbOfPancakes)
                 {
-                    unmetOrders++; // 1 2 3
-                    totalWastedPancakes += pancakesEaten[user] - numbOfPancakes;  // t +=1
+                    unmetOrders++;
+                    totalWastedPancakes[0] += pancakesEaten[user] - numbOfPancakes;
 
                 } else {
-                    numbOfPancakes -= pancakesEaten[user];  // 4
-                    totalPancakesEaten += pancakesEaten[user];  //
+                  //  numbOfPancakes -= pancakesEaten[user];
+                    totalPancakesEaten += pancakesEaten[user];
 
                 }
             }
 
-            totalPancakesMade += numbOfPancakes;  //
+
+            int pancakesEatenByUsers = 0;
+
+            for (int pancakesEat : pancakesEaten) {
+
+                    pancakesEatenByUsers += pancakesEat;
+
+            }
+
+            totalPancakesMade += numbOfPancakes;
             totalUnmetOrders += unmetOrders;
 
             System.out.println("30-Second time Slot# " +time);
             System.out.println("Starting Time of making the pancakes: " + ((time - 1) * 30) + " seconds");
             System.out.println("Ending Time of making the pancakes: " + (time * 30) + " seconds");
             System.out.println("Pancakes Made by Shopkeeper: " + numbOfPancakes);
-            System.out.println("Pancakes Eaten by  Users: " + totalPancakesEaten);
+            System.out.println("Pancakes Eaten by  User: " + pancakesEatenByUsers);
 
             System.out.println("Unmet Orders: " + unmetOrders);
-            System.out.println("Wasted Pancakes: " + (pancakesEaten[0] - numbOfPancakes));
+            System.out.println("Wasted Pancakes: " + (Math.max(0, numbOfPancakes - pancakesEatenByUsers)));
             System.out.println();
         }
 
         System.out.println("Total Pancakes made by the Shopkeeper: " + totalPancakesMade);
-        System.out.println("Total Pancakes Eaten: " + totalPancakesEaten);
+        System.out.println("Total Pancakes Eaten by Users: " + totalPancakesEaten);
         System.out.println("Total Unmet Orders: " + totalUnmetOrders);
-        System.out.println("Total Wasted Pancakes: " + totalWastedPancakes);
 
     }
 }
